d2e935f2ce776c42afd2b6a334c86134b510ad24
Gusb3ll <Kitpipat.0647@gmail.com>
2021-02-26 17:40:04 +0700

    Add response message <3

--- bot.py
+++ bot.py
@@ -16,7 +16,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!a'):
+    if message.content.startswith('Hi'):
         await message.channel.send('Hello, my master!')
+
+    if message.content.startswith('Murasame'):
+        await message.channel.send('Yes?')
+
+    if message.content.startswith('Do you want to stay with me?'):
+        await message.channel.send('Well, master...')
+        await message.channel.send('I am sure we can....')
+        await message.channel.send('Hehe hehe')
 
 client.run(TOKEN)
