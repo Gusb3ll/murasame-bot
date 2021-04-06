5f2e75878c55876cc2cd8720c5d2755e783595f9
Gusb3ll <Kitpipat.0647@gmail.com>
2021-04-06 21:25:38 +0700

    Revoke old code and add path to picture

--- etc/picture.py
+++ etc/picture.py
@@ -1,7 +1,7 @@
-H_pictures = ['Murasame1.png', 'Murasame2.png', 'Murasame3.png', 'Murasame4.png', 'Murasame5.png', 'Murasame6.png', 'Murasame7.png', 'Murasame8.png',
-              'Ayase1.png', 'Ayase2.png', 'Ayase3.png', 'Ayase4.png', 'Ayase5.png',
-              'Hazuki1.png',
-              'Mako1.png', 'Mako2.png',
-              'Mayu1.png', 'Mayu2.png', 'Mayu3.png']
+H_pictures = ['pic/Murasame1.png', 'pic/Murasame2.png', 'pic/Murasame3.png', 'pic/Murasame4.png', 'pic/Murasame5.png', 'pic/Murasame6.png', 'pic/Murasame7.png', 'pic/Murasame8.png',
+              'pic/Ayase1.png', 'pic/Ayase2.png', 'pic/Ayase3.png', 'pic/Ayase4.png', 'pic/Ayase5.png',
+              'pic/Hazuki1.png',
+              'pic/Mako1.png', 'pic/Mako2.png',
+              'pic/Mayu1.png', 'pic/Mayu2.png', 'pic/Mayu3.png']
 
 TEST_pic = ['pic/Ayase1.png', 'pic/Ayase2.png', 'pic/Ayase3.png', 'pic/Ayase4.png']
